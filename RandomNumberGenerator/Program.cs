412945f0541bf9ef3a5811df0800226c47bd8ce8
CloverField <blazelion3@gmail.com>
2014-07-05 22:01:21 -0400

    A basic random numbner generator for steam games, give you a number between 0 and 20.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            System.Console.WriteLine("Your random number is " + rand.Next(1, 20) + ".");
+            System.Console.WriteLine("Press enter to terminate the program.");
+            System.Console.ReadLine();
+        }
+    }
+}
